ace3cd33f2dc79cd590ad498bbccd2b16f3fc1e6
Ilija <168022732+noilija@users.noreply.github.com>
2025-03-07 11:44:26 +0100

    Add files via upload
    This is just a start of python project. BeeMath is game where you solve math problems and get points from solving them correctly.

---
+++ BeeMath/beemath.py
@@ -0,0 +1,5 @@
+import operater
+
+op=operater.Operater()
+
+op.startGame()
---
+++ BeeMath/operater.py
@@ -0,0 +1,42 @@
+import time
+
+class Operater:
+    def __init__(self):
+        pass
+
+    def startGame():
+        Operater.printMenu()
+        
+
+    def gameInProgress():
+        Operater.startGame()
+
+    def endGame():
+        pass
+
+    def timer():
+        pass
+
+    def checkAnswer():
+        pass
+    
+    def showQuestion():
+        pass
+    
+    def showResult():
+        pass
+
+    def showScore():
+        pass
+
+    def printMenu():
+        print("Main Menu:")
+        print("1. Start Game")
+        print("2. Exit")
+        print("3. Help")
+
+    def loadProblem():
+        pass
+    
+    def openFile():
+        pass
